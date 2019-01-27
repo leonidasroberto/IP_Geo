086851d0015e4f3fdeb452b33c7f6a2a3a2433f7
Leonidas Roberto B. J <47005873+leonidasroberto@users.noreply.github.com>
2019-01-26 22:11:36 -0300

    Update ip_geo.py

--- ip_geo.py
+++ ip_geo.py
@@ -5,23 +5,28 @@ import json
 import clipboard
 
 trum=clipboard.paste()
+dart=(trum[3:4])
 
-req=requests.get('http://ipinfo.io/{}'. format(trum))
+if dart != ".":
+  print("Você não copiou um número IP válido!")
 
-trat=json.loads(req.text)
+else:
+  req=requests.get('http://ipinfo.io/{}'. format(trum))
 
-print("")
-ip=trat['ip']
-host=trat['hostname']
-city=trat['city']
-pais=trat['country']
-loc=trat['loc']
+  trat=json.loads(req.text)
 
-print("O ip é:",ip)
-print("Hostname:",host)
-print("Cidade: ",city)
-print("País: ",pais)
-print("Geo Localização: ",loc)
-print("")
-#ATIVE CASO UTILIZE WINDOWS
-#os.system("pause")
+  print("")
+  ip=trat['ip']
+  host=trat['hostname']
+  city=trat['city']
+  pais=trat['country']
+  loc=trat['loc']
+
+  print("O ip é:",ip)
+  print("Hostname:",host)
+  print("Cidade: ",city)
+  print("País: ",pais)
+  print("Geo Localização: ",loc)
+  print("")
+  #ATIVE CASO UTILIZE WINDOWS
+  #os.system("pause")
